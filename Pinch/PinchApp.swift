82bacf20a95593db247642d0605d5981c1da93b7
Md. Shah Faishal <abadlas1987@gmail.com>
2022-03-29 10:43:42 +0530

    Initial Commit

---
+++ Pinch/PinchApp.swift
@@ -0,0 +1,17 @@
+//
+//  PinchApp.swift
+//  Pinch
+//
+//  Created by Scrambleapps on 29/03/22.
+//
+
+import SwiftUI
+
+@main
+struct PinchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
